c3678b77540eb12efda255c5a833a56d3290e050
Vanessa Marques <vanessahelenaeduarda@gmail.com>
2023-04-18 14:10:23 -0300

    Add files via upload

---
+++ Target/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Target
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int INDICE = 13, 
+            SOMA = 0, K = 0;
+
+            while(K < INDICE)
+            {
+                K = K + 1;
+
+                SOMA = SOMA + K;
+
+            }
+            Console.WriteLine(SOMA);
+        } 
+    }  
+}    
+     
+
