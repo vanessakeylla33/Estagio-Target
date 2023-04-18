d6f26602f1722a0ea05ccc21abd33f26c63ed9ab
Vanessa Marques <vanessahelenaeduarda@gmail.com>
2023-04-18 14:13:15 -0300

    Add files via upload

---
+++ reverseString/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace reverseString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string frase = "Amo minha família!";
+            string fraseReversa = "";
+
+            for(int i = frase.Length - 1; i >= 0; i--) 
+            {
+                fraseReversa += frase[i];
+            }
+
+            Console.WriteLine(fraseReversa);
+        }
+    }
+}
